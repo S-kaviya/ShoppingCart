0cc1267f6b8fa2c43f67ee4feba2f900a6e28c3f
Pinjala Supriya <pinjala.supriya@zucisystems.com>
2023-04-09 04:33:01 -0700

    adding header and Footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { BlogComponent } from './blog/blog.component';
+import { PagesComponent } from './pages/pages.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'home', component: HomeComponent},
+  {path:'shop', component: ShopComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: 'pages', component: PagesComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
