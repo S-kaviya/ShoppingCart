004466ad1bbe63a8e65a58bb8238e6a66dca473d
S-kaviya <skaviya1582001@gmail.com>
2023-04-12 22:10:59 +0530

    Fixing Footer and header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ import { KidsClothingComponent } from './kids-clothing/kids-clothing.component';
 
 const routes: Routes = [
 
-  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
   {path:'shop', component: ShopComponent},
   {path: 'blog', component: BlogComponent},
   {path: 'pages', component: PagesComponent},
