c91415b1fd369fdd452faf4a397ad04169765b3e
Pinjala Supriya <pinjala.supriya@zucisystems.com>
2023-04-16 09:56:17 -0700

    adding shop details

--- src/app/Component/shop/shop.component.ts
+++ src/app/Component/shop/shop.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductservicesService } from 'src/app/productservices.service';
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
@@ -7,43 +7,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopComponent {
 
-  products:any[] = [
-    {
-      name: 'Quartz Belt Watch',
-      price: 1399,
-      image: 'https://themewagon.github.io/aroma/img/product/product1.png',
-      category:'Accessories'
-    },
-    {
-      name: 'Women Facewash',
-      price: 1299,
-      image: 'https://themewagon.github.io/aroma/img/product/product2.png',
-      category:'Beauty'
-    },
-    {
-      name: 'Charging Car',
-      price: 1199,
-      image: 'https://themewagon.github.io/aroma/img/product/product6.png',
-      category:'Kids'
-    },
-    {
-      name: 'Bluetooth Speaker',
-      price: 1199,
-      image: 'https://themewagon.github.io/aroma/img/product/product7.png',
-      category:'Accessories'
-    },
-  ];
+//   products:any[] = [
+//     {
+//       name: 'Quartz Belt Watch',
+//       price: 1399,
+//       image: 'https://themewagon.github.io/aroma/img/product/product1.png',
+//       category:'Accessories'
+//     },
+//     {
+//       name: 'Women Facewash',
+//       price: 1299,
+//       image: 'https://themewagon.github.io/aroma/img/product/product2.png',
+//       category:'Beauty'
+//     },
+//     {
+//       name: 'Charging Car',
+//       price: 1199,
+//       image: 'https://themewagon.github.io/aroma/img/product/product6.png',
+//       category:'Kids'
+//     },
+//     {
+//       name: 'Bluetooth Speaker',
+//       price: 1199,
+//       image: 'https://themewagon.github.io/aroma/img/product/product7.png',
+//       category:'Accessories'
+//     },
+//   ];
 
-  productFilter: any = { category: ''};
+//   productFilter: any = { category: ''};
 
-  get categories (){
-    return Array.from(new Set(this.products.map(product => product.category)));
-  }
-  selectedCategory = '';
+//   get categories (){
+//     return Array.from(new Set(this.products.map(product => product.category)));
+//   }
+//   selectedCategory = '';
 
-  get filteredProducts() {
-    return this.products.filter(product => !this.selectedCategory || product.category === this.selectedCategory);
-  }
+//   get filteredProducts() {
+//     return this.products.filter(product => !this.selectedCategory || product.category === this.selectedCategory);
+//   }
   
+
+// }
+allProducts:any[]=[];
+  category:any[]=[];
+  title:string="";
+ price:number=0;
+//  description:string="";
+  image: string="";
+  searchText:string="";
+getTotalProducts: any;
+  
+    //  private _customerService: any;
+  constructor(private ProductservicesService:ProductservicesService){}
+   
+    ngOnInit():void{
+  
+    
+    this.allProducts=this. ProductservicesService.getTotalProducts();
+    }
 
 }
