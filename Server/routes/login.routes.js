cf4785580e1fe25f754b9aab86cd779eee90aaea
S-kaviya <Kaviya.ShanmugaSundaram@alcumus.com>
2023-04-18 14:58:00 +0530

    Register done

--- Server/routes/login.routes.js
+++ Server/routes/login.routes.js
@@ -1,31 +1,65 @@
 const express = require("express");
 const app = express();
-
+let Register = require("../model/Register")
 const loginRoute = express.Router();
-let Login = require('../model/Login');
+let Login = require("../model/Login");
 
+const UserController = require("../Controllers/CU");
 //Add Login details(username and password)
-loginRoute.route('/User-Added').post((req,res,next)=>{
-    Login.create(req.body)
-    .then((data)=>{
-        res.json(data);
+// loginRoute.route('/User-Added').post((req,res,next)=>{
+//     Login.create(req.body)
+//     .then((data)=>{
+//         res.json(data);
+//     })
+//     .catch((error)=>{
+//         return next(error);
+//     });
+// });
+
+// //Add Register details(username and password)
+// loginRoute.route('/Register-User').post((req,res,next)=>{
+//   Login.create(req.body)
+//   .then((data)=>{
+//       res.json(data);
+//   })
+//   .catch((error)=>{
+//       return next(error);
+//   });
+// });
+
+// //Get all user details
+// loginRoute.route('/').get((req, res, next) => {
+//     Login.find()
+//       .then((data) => {
+//         res.json(data);
+//       })
+//       .catch((error) => {
+//         return next(error);
+//       });
+//   });
+
+// module.exports = loginRoute;
+
+loginRoute.post("/User-Added", async (req, res) => {
+  console.log(req.body);
+  const { username, password } = req.body;
+  console.log(username, password);
+  const result = await UserController.checkUser(username, password);
+  console.log("result", result);
+  if (result.error) {
+    return res.status(401).json({ error: result.error });
+  }
+  return res.status(200).json({ user: result, message: "User valid" });
+});
+
+loginRoute.route("/Register-User").post((req, res, next) => {
+  Register.create(req.body)
+    .then((data) => {
+      res.json({ data: data, message: "User Registered Successfully" });
     })
-    .catch((error)=>{
-        return next(error);
+    .catch((error) => {
+      return next(error);
     });
 });
 
-
-//Get all user details
-loginRoute.route('/').get((req, res, next) => {
-    Login.find()
-      .then((data) => {
-        res.json(data);
-      })
-      .catch((error) => {
-        return next(error);
-      });
-  });
-
-  
 module.exports = loginRoute;
--- src/app/Component/register/register.component.ts
+++ src/app/Component/register/register.component.ts
@@ -1,15 +1,95 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+  AbstractControl,
+} from "@angular/forms";
+import { CrudService } from "src/app/service/crud.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"],
 })
 export class RegisterComponent implements OnInit {
+  regForm: FormGroup = new FormGroup({
+    username: new FormControl("", [
+      Validators.required,
+      Validators.pattern("[a-zA-Z].*"),
+    ]),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    phone: new FormControl("", [
+      Validators.required,
+      Validators.pattern("[0-9]*"),
+      Validators.minLength(10),
+      Validators.maxLength(10),
+    ]),
+    password: new FormControl("", [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(12),
+    ]),
+    confirmpassword: new FormControl("", [Validators.required]),
+  });
+  constructor(
+    private Rf: FormBuilder,
+    public router: Router,
+    private ngZone: NgZone,
+    private LoginService: CrudService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get username(): FormControl {
+    return this.regForm.get("username") as FormControl;
   }
+  get email(): FormControl {
+    return this.regForm.get("email") as FormControl;
+  }
+  get mobile(): FormControl {
+    return this.regForm.get("phone") as FormControl;
+  }
+  get pwd(): FormControl {
+    return this.regForm.get("password") as FormControl;
+  }
+  get cpwd(): FormControl {
+    return this.regForm.get("confirmpassword") as FormControl;
+  }
+  ngOnInit(): void {}
+  repeatPass: String = "none";
+  successMessage: String = "";
+  messageShow: Boolean = false;
 
+  onSubmit() {
+    if (this.pwd.value === this.cpwd.value) {
+      this.repeatPass = "none";
+      console.log("correct password");
+    } else {
+      this.repeatPass = "inline";
+    }
+    console.log(this.regForm.value);
+
+    if (this.regForm.status === "VALID" && this.pwd.value === this.cpwd.value) {
+      this.LoginService.RegisterUser(this.regForm.value).subscribe(
+        (res) => {
+          console.log("Response", res);
+
+          this.successMessage = res.message;
+
+          this.regForm.reset();
+
+          // this.ngZone.run(() => this.router.navigateByUrl("/Login"));
+        },
+
+        (err) => {
+          console.log("err", err);
+
+          this.successMessage = err;
+        }
+      );
+    } else {
+      console.log("error from register ts");
+    }
+  }
 }
